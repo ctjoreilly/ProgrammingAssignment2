0a08b00d4bbac26e0daeff102a73ed4eeec6fe24
Ciaran O'Reilly <ctjoreilly@gmail.com>
2015-09-24 08:03:05 -0700

    Add initial implementation of cache matrix logic.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
+## TODO
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## TODO
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x   <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
+## TODO
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix, ...)
+    x$setinverse(inv)
+    inv
+}
+
+testCacheMatrix <- function() {
+    ## TODO - implements reproducible tests
+    print("Test 1:")
 }
